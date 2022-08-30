fbb7114fce6827eb2ecc78499ad5e133d6940824
Richard Minsoo <67932257+RichardMinsooGo-RL-Gym@users.noreply.github.com>
2022-08-30 13:39:09 +0900

    Add files via upload

--- TF2_A_VI_31_per_dd_dqn.py
+++ TF2_A_VI_31_per_dd_dqn.py
@@ -1,13 +1,5 @@
-import tensorflow as tf
-import numpy as np
+# IMPORTING LIBRARIES
 
-from tensorflow.keras import optimizers, losses
-from tensorflow.keras import Model
-from collections import deque
-
-import collections
-import random
-import gym
 
 class SumTree:
     write = 0
@@ -103,16 +95,34 @@ class PrioritizedReplayBuffer(object):  # stored as ( s, a, r, s_ ) in SumTree
     def update(self, idx, error):
         p = self._getPriority(error)
         self.tree.update(idx, p)
+import sys
+IN_COLAB = "google.colab" in sys.modules
 
-class DQN(Model):
-    def __init__(self):
-        super(DQN, self).__init__()
+import random
+import gym
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras import optimizers, losses
+from tensorflow.keras import Model
+from collections import deque
+
+from IPython.display import clear_output
+
+
+# CREATING THE Q-Network
+# Neural Network Model Defined at Here.
+class Network(Model):
+    def __init__(self, state_size: int, action_size: int, 
+    ):
+        """Initialization."""
+        super(Network, self).__init__()
         
-        self.num_action = 2
-        self.layer1 = tf.keras.layers.Dense(64, activation='relu')
-        self.layer2 = tf.keras.layers.Dense(64, activation='relu')
-        self.state = tf.keras.layers.Dense(self.num_action)
-        self.action = tf.keras.layers.Dense(self.num_action)
+        self.action_size = action_size
+        self.layer1 = tf.keras.layers.Dense(hidden_size, activation='relu')
+        self.layer2 = tf.keras.layers.Dense(hidden_size, activation='relu')
+        self.state = tf.keras.layers.Dense(self.action_size)
+        self.action = tf.keras.layers.Dense(self.action_size)
 
     def call(self, state):
         layer1 = self.layer1(state)
@@ -124,52 +134,84 @@ class DQN(Model):
         value = state + advantage        
         return value
 
-class Agent:
-    def __init__(self):
+class DQNAgent:
+    def __init__(
+        self, 
+        env: gym.Env,
+        batch_size: int,
+        target_update: int,
+    ):
+        """Initialization.
+        
+        Args:
+            env (gym.Env): openAI Gym environment
+            memory_size (int): length of memory
+            batch_size (int): batch size for sampling
+            target_update (int): period for target model's hard update
+            epsilon_decay (float): step size to decrease epsilon
+            lr (float): learning rate
+            max_epsilon (float): max value of epsilon
+            min_epsilon (float): min value of epsilon
+            gamma (float): discount factor
+        """
+        
+        # CREATING THE Q-Network
+        self.env = env
+        
+        self.state_size = self.env.observation_space.shape[0]
+        self.action_size = self.env.action_space.n
+        
+        self.batch_size = batch_size
         # hyper parameters
         self.lr = 0.001
-        self.gamma = 0.99
-
-        self.dqn_model = DQN()
-        self.dqn_target = DQN()
-        self.opt = optimizers.Adam(lr=self.lr, )
-
-        self.batch_size = 64
-        self.state_size = 4
-        self.action_size = 2
-
+        self.target_update = target_update
+        self.gamma = 0.99    # discount rate
+        
+        # create main model and target model
+        self.dqn = Network(self.state_size, self.action_size
+                          )
+        self.dqn_target = Network(self.state_size, self.action_size
+                          )
+        self.optimizers = optimizers.Adam(lr=self.lr, )
+        
         self.memory = PrioritizedReplayBuffer(capacity=2000)
-
-    def update_target(self):
-        self.dqn_target.set_weights(self.dqn_model.get_weights())
-    
+        
+        self._target_hard_update()
+        
+    # 3.4.1 EXPLORATION VS EXPLOITATION
     def get_action(self, state, epsilon):
-        q_value = self.dqn_model(tf.convert_to_tensor([state], dtype=tf.float32))
+        q_value = self.dqn(tf.convert_to_tensor([state], dtype=tf.float32))
+        # 3. Choose an action a in the current world state (s)
+        # If this number < greater than epsilon doing a random choice --> exploration
         if np.random.rand() <= epsilon:
             action = np.random.choice(self.action_size)
+
+        ## Else --> exploitation (taking the biggest Q value for this state)
         else:
             action = np.argmax(q_value) 
-        return action, q_value
 
+        return action
+    
     def append_sample(self, state, action, reward, next_state, done):
         state = tf.convert_to_tensor([state], dtype=tf.float32)
         next_state = tf.convert_to_tensor([next_state], dtype=tf.float32)
 
-        main_next_q = np.array(self.dqn_model(next_state))[0]
+        main_next_q = np.array(self.dqn(next_state))[0]
         next_action = np.argmax(main_next_q)
         target_next_q = np.array(self.dqn_target(next_state))[0]
         target_value = target_next_q[next_action]
 
         target_value = target_value * 0.99 * (1-done) + reward
 
-        main_q = np.array(self.dqn_model(state))[0]
-        main_q = main_q[action]
+        curr_Qs = np.array(self.dqn(state))[0]
+        curr_Qs = curr_Qs[action]
 
-        td_error = np.abs(target_value - main_q)
+        td_error = np.abs(target_value - curr_Qs)
 
         self.memory.add(td_error, (state, action, reward, next_state, done))
 
-    def update(self):
+    # 3.4.2 UPDATING THE Q-VALUE
+    def train_step(self):
         mini_batch, idxs, IS_weight = self.memory.sample(self.batch_size)
         mini_batch = np.array(mini_batch)
         states      = [i[0] for i in mini_batch]
@@ -177,34 +219,37 @@ class Agent:
         rewards     = [i[2] for i in mini_batch]
         next_states = [i[3] for i in mini_batch]
         dones       = [i[4] for i in mini_batch]
-
-        dqn_variable = self.dqn_model.trainable_variables
+        
+        dqn_variable = self.dqn.trainable_variables
         with tf.GradientTape() as tape:
             tape.watch(dqn_variable)
             
-            rewards = tf.convert_to_tensor(rewards, dtype=tf.float32)
-            actions = tf.convert_to_tensor(actions, dtype=tf.int32)
-            dones = tf.convert_to_tensor(dones, dtype=tf.float32)
-
-            target_q = self.dqn_target(tf.convert_to_tensor(np.vstack(next_states), dtype=tf.float32))
-            main_q = self.dqn_model(tf.convert_to_tensor(np.vstack(next_states), dtype=tf.float32))
-            main_q = tf.stop_gradient(main_q)
-            next_action = tf.argmax(main_q, axis=1)
-            target_value = tf.reduce_sum(tf.one_hot(next_action, self.action_size) * target_q, axis=1)
-
-            target_value = (1-dones) * self.gamma * target_value + rewards
-
-            main_q = self.dqn_model(tf.convert_to_tensor(np.vstack(states), dtype=tf.float32))
-            main_value = tf.reduce_sum(tf.one_hot(actions, self.action_size) * main_q, axis=1)
-
+            states      = tf.convert_to_tensor(np.vstack(states), dtype=tf.float32)
+            actions     = tf.convert_to_tensor(actions, dtype=tf.int32)
+            rewards     = tf.convert_to_tensor(rewards, dtype=tf.float32)
+            next_states = tf.convert_to_tensor(np.vstack(next_states), dtype=tf.float32)
+            dones       = tf.convert_to_tensor(dones, dtype=tf.float32)
+            
+            curr_Qs    = self.dqn(states)
+            next_Q_targs = self.dqn_target(next_states)
+            curr_Qs = tf.stop_gradient(curr_Qs)
+            next_action = tf.argmax(curr_Qs, axis=1)
+            target_value = tf.reduce_sum(tf.one_hot(next_action, self.action_size) * next_Q_targs, axis=1)
+            
+            mask = 1 - dones
+            target_value = rewards + self.gamma * target_value * mask 
+            
+            curr_Qs = self.dqn(states)
+            main_value = tf.reduce_sum(tf.one_hot(actions, self.action_size) * curr_Qs, axis=1)
+            
             error = tf.square(main_value - target_value) * 0.5
             error = error * tf.convert_to_tensor(IS_weight, dtype=tf.float32)
-            error = tf.reduce_mean(error)
+            loss  = tf.reduce_mean(error)
             
-        dqn_grads = tape.gradient(error, dqn_variable)
-        self.opt.apply_gradients(zip(dqn_grads, dqn_variable))
+        dqn_grads = tape.gradient(loss, dqn_variable)
+        self.optimizers.apply_gradients(zip(dqn_grads, dqn_variable))
 
-        state_value = np.array(self.dqn_model(tf.convert_to_tensor(np.vstack(states), dtype=tf.float32)))
+        state_value = np.array(self.dqn(tf.convert_to_tensor(np.vstack(states), dtype=tf.float32)))
         state_value = np.array([sv[a] for a, sv in zip(np.array(actions), state_value)])
 
         td_error = np.abs(target_value - state_value)
@@ -213,33 +258,87 @@ class Agent:
             idx = idxs[i]
             self.memory.update(idx, td_error[i])            
 
-    def run(self):
+    def _target_hard_update(self):
+        """Hard update: target <- local."""
+        self.dqn_target.set_weights(self.dqn.get_weights())
 
-        env = gym.make('CartPole-v0')
-        episode = 0
-        step = 0
+# CREATING THE ENVIRONMENT
+env_name = "CartPole-v0"
+env = gym.make(env_name)
 
-        for episode in range(500):
-            state = env.reset()
-            done = False
-            episode += 1
-            epsilon = 1 / (episode * 0.1 + 1)
-            score = 0
-            while not done:
-                step += 1
-                action, q_value = self.get_action(state, epsilon)
-                next_state, reward, done, info = env.step(action)
-                self.append_sample(state, action, reward, next_state, done)
-                score += reward
+# parameters
+target_update = 100
 
-                state = next_state
 
-                if step > 1000:
-                    self.update()
-                    if step % 20 == 0:
-                        self.update_target()
-            print(episode, score)
+# INITIALIZING THE Q-PARAMETERS
+hidden_size = 128
+max_episodes = 200  # Set total number of episodes to train agent on.
+batch_size = 64
 
-if __name__ == '__main__':
-    agent = Agent()
-    agent.run()
+# Exploration parameters
+epsilon = 1.0                 # Exploration rate
+max_epsilon = 1.0             # Exploration probability at start
+min_epsilon = 0.01            # Minimum exploration probability 
+decay_rate = 0.005            # Exponential decay rate for exploration prob
+
+# train
+agent = DQNAgent(
+    env, 
+#     memory_size, 
+    batch_size, 
+    target_update, 
+#     epsilon_decay,
+)
+
+if __name__ == "__main__":
+    
+    update_cnt    = 0
+    # 2.5 TRAINING LOOP
+    #List to contain all the rewards of all the episodes given to the agent
+    scores = []
+    
+    # 2.6 EACH EPISODE    
+    for episode in range(max_episodes):
+        ## Reset environment and get first new observation
+        state = agent.env.reset()
+        episode_reward = 0
+        done = False  # has the enviroment finished?
+        
+            
+        # 2.7 EACH TIME STEP    
+        while not done:
+        # for step in range(max_steps):  # step index, maximum step is 99
+        
+            # 3.4.1 EXPLORATION VS EXPLOITATION
+            # Take the action (a) and observe the outcome state(s') and reward (r)
+            action = agent.get_action(state, epsilon)
+            
+            # 2.7.2 TAKING ACTION
+            next_state, reward, done, _ = agent.env.step(action)
+            agent.append_sample(state, action, reward, next_state, done)
+            
+            # Our new state is state
+            state = next_state
+
+            episode_reward += reward
+
+            # if episode ends
+            if done:
+                scores.append(episode_reward)
+                print("Episode " + str(episode+1) + ": " + str(episode_reward))
+                break
+            update_cnt += 1
+            # if training is ready
+            if update_cnt >= 1000:
+                # 3.4.2 UPDATING THE Q-VALUE
+                agent.train_step()
+            
+                # if hard update is needed
+                if update_cnt % agent.target_update == 0:
+                    agent._target_hard_update()
+            
+        # 2.8 EXPLORATION RATE DECAY
+        # Reduce epsilon (because we need less and less exploration)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode) 
+
+        
